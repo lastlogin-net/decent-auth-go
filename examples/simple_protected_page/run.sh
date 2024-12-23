abc84271638d1742b695bd2fd61616fe80e73d66
Anders Pitman <&ers@apitman.com>
2024-12-23 11:52:21 -0700

    Rename minimal example

--- examples/simple_protected_page/run.sh
+++ examples/simple_protected_page/run.sh
@@ -4,4 +4,4 @@ set -e
 #cp ../../../decent-auth-rs/target/wasm32-wasip1/release/decent_auth_rs.wasm ../../decent_auth.wasm
 cp ../../../decent-auth-rs/target/wasm32-wasip1/release/decentauth.wasm ../../decent_auth.wasm
 go build
-./minimal $@
+./simple_protected_page $@
